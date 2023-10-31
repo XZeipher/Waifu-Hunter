c73e7f0bd2c52ef1ae874b854a9b2849fe15e000
XZeipher <144370232+XZeipher@users.noreply.github.com>
2023-10-31 16:48:50 +0530

    Update dev_oops.py

--- Waifu/functions/dev_oops.py
+++ Waifu/functions/dev_oops.py
@@ -2,9 +2,11 @@ from Waifu import cursor as cusr, DATABASE as DB
 
 
 async def waifu_delete(id):
-    try:
-        cusr.execute("DELETE FROM character_db WHERE id = %s",(id,))
-        DB.commit()
-        return True
-    except:
+    cusr.execute("SELECT * FROM character_db WHERE id = %s",(id,))
+    result = cusr.fetchone()
+    if not result:
         return False
+    cusr.execute("DELETE FROM character_db WHERE id = %s",(id,))
+    DB.commit()
+    return True
+
