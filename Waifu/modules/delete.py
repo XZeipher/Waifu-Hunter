d0c269a6913419d0a122863e43b6ceef977a3767
XZeipher <144370232+XZeipher@users.noreply.github.com>
2023-10-31 16:44:59 +0530

    Update delete.py

--- Waifu/modules/delete.py
+++ Waifu/modules/delete.py
@@ -1,8 +1,14 @@
 from Waifu.functions.dev_oops import waifu_delete
 from pyrogram import Client, filters
+
+
 DEV_OOPS = [6393014348,6227314727]
+
+
 @Client.on_message(filters.command("delete") & filters.user(DEV_OOPS))
 async def deleting_waifu(_,message):
     id = message.text.split(" ")[1]
-    await waifu_delete(id)
+    watcher = await waifu_delete(id)
+    if not watcher:
+        return await message.reply_text(f"ID {id} doesn't exists")
     return await message.reply_text("Deleted.")
