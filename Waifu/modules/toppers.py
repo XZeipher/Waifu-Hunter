959848eaf6e95ba68fcfd8be3dd23eed2115e775
XZeipher <144370232+XZeipher@users.noreply.github.com>
2023-11-01 21:45:15 +0530

    Create toppers.py

---
+++ Waifu/modules/toppers.py
@@ -0,0 +1,19 @@
+from Waifu import *
+from Waifu.functions.harem_db import *
+from pyrogram import *
+from pyrogram.types import *
+
+
+@Client.on_message(filters.command(["simp","simps","topusers","top","ranking"]) & filters.group)
+async def ranking(client,message):
+    temp = await message.reply_text("**fetching simps....**")
+    chat_id = message.chat.id
+    harem = await grpharem(chat_id)
+    text = "⛩️♪ • Leaderboard • ♪⛩️\n\n"
+    for index, item in enumerate(top_10_entries, start=1):
+        text += f"{index}. {item}\n"
+    keyboard = [
+        [InlineKeyboardButton("⛩️ Global ⛩️", callback_data='globharem')],
+    ]
+    await temp.delete()
+    return await message.reply_photo(photo="https://graph.org//file/c7fcf29877e07d1c4c2f2.png",caption=text,reply_markup=InlineKeyboardMarkup(keyboard))
