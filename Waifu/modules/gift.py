82b2af783405870c2c3be716ded01509d0cfd512
XZeipher <144370232+XZeipher@users.noreply.github.com>
2023-11-01 13:01:09 +0530

    Update gift.py

--- Waifu/modules/gift.py
+++ Waifu/modules/gift.py
@@ -7,7 +7,7 @@ from pyrogram.types import *
 
 @Client.on_message(filters.command("gift") & filters.group)
 async def gifting(client,message):
-    if len(message.text) < 4:
+    if not message.text.split(" ")[1]:
         return await message.reply_text("provide an waifu id")
     id = message.text.split(" ")[1]
     if not int(id):
