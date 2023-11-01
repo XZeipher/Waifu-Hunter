98a2e8051092d47b2ca4bb2960b9598a88f858c3
XZeipher <144370232+XZeipher@users.noreply.github.com>
2023-11-01 15:31:53 +0530

    Update gift.py

--- Waifu/modules/gift.py
+++ Waifu/modules/gift.py
@@ -39,8 +39,7 @@ async def cancel(client,query):
     user_ids = data.split("_")[1]
     if int(user_ids) != user_id:
         return await query.answer("Sorry dear,\nbut this isn't your waifu.",show_alert=True)
-    await query.message.delete()
-    return await query.message.reply_text("Cancelled ❌")
+    return await query.edit_message_caption("Cancelled ❌")
 
 @app.on_callback_query(filters.regex(r"^confirmgift_"))
 async def confim_fuf(client,query):
