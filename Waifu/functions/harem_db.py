dec8992d1c659299d764c70d051ec5976fe326ce
XZeipher <144370232+XZeipher@users.noreply.github.com>
2023-11-02 09:17:12 +0530

    Update harem_db.py

--- Waifu/functions/harem_db.py
+++ Waifu/functions/harem_db.py
@@ -1,18 +1,19 @@
 from Waifu import cusr,DB,app
 
-async def get_users(item):
-    try:
-        search = await app.get_users(int(item))
-    except:
-        return item
-    return search.mention
+
 async def globrank():
     cusr.execute("SELECT * FROM user_data")
     data = cusr.fetchall()
-    sorted_waifu_data = sorted(data, key=lambda x: int(x[-1]), reverse=True)
-    formatted_list = [f"{item[1]} • {item[-1]}" for item in sorted_waifu_data]
-    top_10_entries = formatted_list[:10]
-    return top_10_entries
+    count = {}
+    alt = []
+    for item in data:
+        user_id = item[1]
+        if user_id in count:
+            count[user_id] += 1
+        else:
+            count[user_id] = 1
+    sorted_users = sorted(count.items(), key=lambda x: x[1], reverse=True)[:10]
+    return sorted_users
 
 async def grpharem(chat_id):
     alt = []
