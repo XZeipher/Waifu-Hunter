e3d86c83d05e1b0e5af7329fd2263537ff2f0ed9
XZeipher <144370232+XZeipher@users.noreply.github.com>
2023-11-02 09:27:39 +0530

    Update toppers.py

--- Waifu/modules/toppers.py
+++ Waifu/modules/toppers.py
@@ -18,3 +18,31 @@ async def ranking(client,message):
     ]
     await temp.delete()
     return await message.reply_photo(photo="https://graph.org//file/c7fcf29877e07d1c4c2f2.png",caption=text,reply_markup=InlineKeyboardMarkup(keyboard))
+
+@app.on_callback_query(filters.regex(r"^(globharem|grpharem)$"))
+async def harem_ranking(client,query):
+    data = query.data
+    chat_id = query.message.chat.id
+    keyboard = [
+        [InlineKeyboardButton("⛩️ Group ⛩️", callback_data='grpharem')],
+    ]
+    keyboards = [
+        [InlineKeyboardButton("⛩️ Global ⛩️", callback_data='globharem')],
+    ]
+    if data == "globharem":
+        text = "**⛩️♪ • Leaderboard • ♪⛩️\n\n**"
+        await query.answer()
+        rank = await globrank()
+        for index, (user_id, count) in enumerate(rank, start=1):
+            user = await client.get_users(int(user_id))
+            text += f"{index}. {user.mention} • {count}\n"
+        return await query.edit_message_caption(text,reply_markup=InlineKeyboardMarkup(keyboard))
+    elif data == "grpharem":
+        text = "**⛩️♪ • Leaderboard • ♪⛩️\n\n**"
+        await query.answer()
+        data = await grpharem(chat_id)
+        for index, (user_id, count) in enumerate(data, start=1):
+            user = await client.get_users(int(user_id))
+            text += f"{index}. {user.mention} • {count}\n"
+        return await query.edit_message_caption(text,reply_markup=InlineKeyboardMarkup(keyboards))
+    
