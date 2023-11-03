c4782ad6a25fcb678ccd098dd35eb84501ac55a7
XZeipher <144370232+XZeipher@users.noreply.github.com>
2023-11-03 22:16:59 +0530

    Create on_message.py

---
+++ Waifu/modules/on_message.py
@@ -0,0 +1,47 @@
+"""
+MIT License
+
+Copyright (c) 2023 XZeipher
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+from pyrogram import *
+from Waifu import *
+from waifu.functions.stats_db import add_chat
+
+new_chat = """
+{} 
+#NEWCHAT
+CHAT : {}
+"""
+
+@app.on_message(filters.new_chat_members)
+async def new_mem(_,message):
+    for member in message.new_chat_members:
+        id = member.id
+    apps = await app.get_me()
+    bot_id = apps.id
+    bot_name = apps.first_name
+    if int(bot_id) == int(id):
+        chatting = await add_chat(chat_id)
+        if chatting:
+            return await app.send_message(-1001685819877,new_chat.format(bot_name,message.chat.username))
+        return
+    return
