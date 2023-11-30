db9dd38c6dab37be323c38bce0284dd1a03b2866
XZeipher <144370232+XZeipher@users.noreply.github.com>
2023-11-30 07:37:17 +0530

    Update harem.py

--- Waifu/modules/harem.py
+++ Waifu/modules/harem.py
@@ -82,9 +82,8 @@ async def handle_inline_query(query):
                 ))
             total_results = len(results)
             current_page = int(query.offset) if query.offset else 0
-            items_per_page = 10
+            items_per_page = 50
             next_offset = current_page + items_per_page if current_page + items_per_page < total_results else None
-
             await query.answer(results[current_page:current_page+items_per_page], cache_time=0, is_gallery=True, next_offset=str(next_offset))
         else:
             message = "You have no characters."
