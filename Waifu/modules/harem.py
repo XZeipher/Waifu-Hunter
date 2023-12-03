12b5421f9e3ef8bfeb18a718d6e2d247284cedd2
XZeipher <144370232+XZeipher@users.noreply.github.com>
2023-12-03 20:52:29 +0530

    Update harem.py

--- Waifu/modules/harem.py
+++ Waifu/modules/harem.py
@@ -95,6 +95,26 @@ async def handle_inline_query(query):
                 title="Not Found",
                 input_message_content=InputTextMessageContent(message)
             )], cache_time=0)
+    elif query.query.startswith("winter"):
+        cursor.execute("SELECT * FROM winter_characters")
+        fetched = cursor.fetchall()
+        results = []
+        for rex in fetched:
+            id,name,anime,rarity,pic = rex
+            cap = "**OwO! Check out this qt waifu!\n\n**"
+            cap += f"**🌅{anime}\n**"
+            cap += f"**💮 Name : {name}\n**"
+            cap += f"**🫧 Rarity : {rarity}\n**"
+            results.append(InlineQueryResultPhoto(
+                photo_url=pic,
+                thumb_url=pic,
+                caption=cap
+            ))
+        total_results = len(results)
+        current_page = int(query.offset) if query.offset else 0
+        items_per_page = 50
+        next_offset = current_page + items_per_page if current_page + items_per_page < total_results else None
+        await query.answer(results[current_page:current_page+items_per_page], cache_time=0, is_gallery=True, next_offset=str(next_offset))
     else:
         cursor.execute("SELECT * FROM character_db")
         fetched = cursor.fetchall()
