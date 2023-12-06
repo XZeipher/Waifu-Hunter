2ceb5037ac120ce41bd20b60377e37ff5db86212
XZeipher <144370232+XZeipher@users.noreply.github.com>
2023-12-06 08:08:55 +0530

    Update events_db.py

--- Waifu/functions/events_db.py
+++ Waifu/functions/events_db.py
@@ -31,6 +31,15 @@ cusr.execute("""
     )
 """)
 DB.commit()
+cusr.execute("""
+    CREATE TABLE IF NOT EXISTS gban_db (
+        id SERIAL PRIMARY KEY,
+        user_id VARCHAR(255) NOT NULL,
+        username TEXT NOT NULL,
+        reason TEXT NOT NULL
+    )
+""")
+DB.commit()
 cursor.execute("""
     CREATE TABLE IF NOT EXISTS winter_characters (
         id SERIAL PRIMARY KEY,
@@ -41,6 +50,25 @@ cursor.execute("""
     )
 """)
 DATABASE.commit()
+
+async def gban_check(user_id):
+    cusr.execute("SELECT * FROM gban_db WHERE user_id = %s",(str(user_id),))
+    results = cusr.fetchone()
+    if results:
+        return {"user_id":int(results[1]) , "username":results[2],"reason":results[3]}
+    return False
+
+async def add_gban(user_id,username,reason):
+    cusr.execute("INSERT INTO gban_db (user_id,username,reason) VALUES (%s,%s,%s)",(str(user_id),username,reason,))
+    DB.commit()
+    return True
+
+async def rm_gban(user_id):
+    cusr.execute("DELETE FROM gban_db WHERE user_id = %s",(str(user_id),))
+    DB.commit()
+    return True
+
+    
 
 async def winter_check():
     cusr.execute("SELECT * FROM winter_event WHERE data = %s",("True",))
