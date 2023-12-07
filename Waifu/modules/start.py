b7a7b163632b09f33ebeeccd402fdf15f7c82c09
XZeipher <144370232+XZeipher@users.noreply.github.com>
2023-12-07 16:15:21 +0530

    Update start.py

--- Waifu/modules/start.py
+++ Waifu/modules/start.py
@@ -58,5 +58,5 @@ async def start(client,message):
         if user:
             await app.send_message(-1002103089465,new_user.format(name,message.from_user.mention))
         TEXT = f"Hey {message.from_user.mention}, I know you can't wait to be with your favourite waifus but I only function in a group , so add me there and watch the magic."
-    return await message.reply_photo(photo="https://graph.org//file/e6143909bef2d121d6407.png",caption=TEXT,reply_markup=InlineKeyboardMarkup(BUTT))
+    return await message.reply_photo(photo="https://i.imgur.com/9Ko02O3.jpeg",caption=TEXT,reply_markup=InlineKeyboardMarkup(BUTT))
     
