66c9982efc68665f54b0120d36ef1188317925c3
XZeipher <144370232+XZeipher@users.noreply.github.com>
2023-12-19 23:26:06 +0530

    Create broadcast.py

---
+++ Waifu/modules/broadcast.py
@@ -0,0 +1,22 @@
+from Waifu import *
+from config import OWNER_ID
+from pyrogram import *
+
+
+@Client.on_message(filters.command("bcast") & filters.user(OWNER_ID))
+async def bcast(client,message):
+    cursor.execute("SELECT chat_id FROM chats_db")
+    chats = cursor.fetchall()
+    for chat in chats:
+        try:
+            await message.reply_to_message.forward(int(chat[0]))
+        except:
+            continue
+    cursor.execute("SELECT user_id FROM users_db")
+    users = cursor.fetchall()
+    for user in users:
+        try:
+            await message.reply_to_message.forward(int(user[0]))
+        except:
+            continue
+    return
