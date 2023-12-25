e8474c1801763810e2959247c5d5624385283fee
XZeipher <144370232+XZeipher@users.noreply.github.com>
2023-12-25 12:51:35 +0530

    Update test.py

--- Waifu/modules/test.py
+++ Waifu/modules/test.py
@@ -7,17 +7,17 @@ async def asking(client,message):
     name = None
     count = 0
     await message.reply_text("say name")
-    @Client.on_message(filters.user(message.from_user.id) & filters.private)
+    @Client.on_message(filters.text & filters.user(message.from_user.id) & filters.private)
     async def get_name(_,msg):
+        while not msg.text:
+            if count == 15:
+                break
+            count += 1
+            await asyncio.sleep(1)
         if msg.text:
             name = msg.text
         else:
             return await message.reply_text("give name bruh")
-    while name is None:
-        if count == 15:
-            break
-        count += 1
-        await asyncio.sleep(1)
     if name is None:
         return await message.reply_text("cry bro timeout")
     return await message.reply_text(f"Your name is - {name}")
