a56575b56cec5461472c67ce5725971c5608fb7a
XZeipher <144370232+XZeipher@users.noreply.github.com>
2024-02-12 17:27:50 +0530

    Update adder.py

--- Waifu/modules/adder.py
+++ Waifu/modules/adder.py
@@ -21,7 +21,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
-'''
+
 from Waifu import *
 from pyrogram import *
 from telegraph import Telegraph , upload_file
@@ -38,11 +38,19 @@ cursor.execute("""
 """)
 DATABASE.commit()
 telegraph = Telegraph()
-new_user = telegraph.create_account(short_name="WaifuxBot")
+new_user = telegraph.create_account(short_name="WaifuBot")
 auth_url = new_user["auth_url"]
+PROCESS = {}
 
-@Client.on_message(filters.command("upload") & filters.user(OWNER_ID))
+@Client.on_message(filters.command("upload") & filters.private & filters.user(AUTH_USERS))
 async def adder(client , message):
+    user_id = message.from_user.id
+    if user_id not in PROCESS:
+        PROCESS[user_id] = {'link': None,'rarity': None,'name': None,'anime': None}
+        return await message.reply_text("**Send me the image you want to upload.**")
+    else:
+        return await message.reply_text("**Finish your previous uploading.**")
+    '''
     replied = message.reply_to_message
     if not replied.photo:
         return await message.reply_text("reply to a image")
@@ -66,23 +74,26 @@ async def adder(client , message):
         return await message.reply_text("Invalid Rarity.\nAllowed Rarity : [🟢,🟣,🟡,🔮]")
     cursor.execute("INSERT INTO character_db (name , anime , rarity , pic) VALUES (%s , %s , %s , %s)",(name,anime,rarity,link,))
     DATABASE.commit()
-    return await message.reply_photo(link,caption=f"✨ Added Character in Database.\nName : {name}\nAnime : {anime}\nRarity : {rarity}")
+    return await message.reply_photo(link,caption=f"✨ Added Character in Database.\nName : {name}\nAnime : {anime}\nRarity : {rarity}")'''
 
-@Client.on_message(filters.command("winter") & filters.user([6761575762,5080865224]))
-async def adder_winter(client , message):
-    replied = message.reply_to_message
-    if not replied.photo:
-        return await message.reply_text("reply to a image")
-    if not replied.caption:
-        return await message.reply_text("image has no name in caption")
-    down = await replied.download()
-    pic = upload_file(down)
-    link = f"https://graph.org{pic[0]}"
-    cap = message.reply_to_message.caption
-    name = cap.split("+")[0]
-    anime = cap.split("+")[1]
-    rarity = "🔮 Limited-Time"
-    cursor.execute("INSERT INTO winter_characters (name , anime , rarity , pic) VALUES (%s , %s , %s , %s)",(name,anime,rarity,link,))
-    DATABASE.commit()
-    return await message.reply_photo(link,caption=f"✨ Added Character in Database.\nName : {name}\nAnime :{anime}\nRarity : {rarity}")
-'''
+@app.on_message(filters.private)
+async def processing(client,message):
+    user_id = message.from_user.id
+    if user_id not in PROCESS:
+        return
+    if message.photo:
+        down = await message.download()
+        pic = upload_file(down)
+        link = f"https://graph.org{pic[0]}"
+        PROCESS[user_id]['link'] = link
+        return await message.reply_text("**Please tell me the character name.**")
+    elif PROCESS[user_id]['link'] is not None:
+        name = message.text
+        PROCESS[user_id]['name'] = name
+        return await message.reply_text("**Please tell me the anime name.**")
+    elif PROCESS[user_id]['name'] is not None:
+        anime = message.text
+        PROCESS[user_id]['anime'] = anime
+        return await message.reply_text("**Please tell me the rarity.**")
+    await message.reply_photo(photo=PROCESS[user_id]['link'],caption=f"Name - {PROCESS[user_id]['name']}\nAnime - {PROCESS[user_id]['anime']}")
+    return PROCESS.pop(user_id)
