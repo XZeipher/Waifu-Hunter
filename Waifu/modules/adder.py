0ff2ff6fccc04dea6c6111f6e40c46f2f107b899
XZeipher <144370232+XZeipher@users.noreply.github.com>
2024-02-13 20:43:44 +0530

    Update adder.py

--- Waifu/modules/adder.py
+++ Waifu/modules/adder.py
@@ -24,7 +24,7 @@ SOFTWARE.
 
 from Waifu import *
 from pyrogram import *
-#from pyromod.listen import Client as cli
+from pyromod import listen
 from telegraph import Telegraph , upload_file
 import psycopg2
 
@@ -46,9 +46,9 @@ PROCESS = {}
 @Client.on_message(filters.command("upload") & filters.private & filters.user(AUTH_USERS))
 async def adder(client , message):
     user_id = message.from_user.id
-    chat = message.chat
+    chat = message.chat.id
     try:
-        response = await chat.ask('**Send the waifu picture**',filters=filters.photo)
+        response = await Client.ask(chat_id=chat,text='**Send the waifu picture**',filters=filters.photo)
         return await message.reply(response)
     except Exception as e:
         return await message.reply(str(e))
