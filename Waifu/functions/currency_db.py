d4499adb7d243039c893586280006c35ec511e86
XZeipher <144370232+XZeipher@users.noreply.github.com>
2024-02-20 10:23:35 +0530

    Update currency_db.py

--- Waifu/functions/currency_db.py
+++ Waifu/functions/currency_db.py
@@ -16,7 +16,7 @@ async def add_fragments(user_id,celestialFragments):
         cusr.execute("INSERT INTO fragments (user_id, celestialFragments) VALUES (%s, %s)",(str(user_id),str(celestialFragments),))
         DB.commit()
         return True
-    newAmoumt = int(result[0]) + celestialFragments
+    newAmount = int(result[0]) + celestialFragments
     cusr.execute("UPDATE fragments SET celestialFragments = %s WHERE user_id = %s",(str(newAmount) , str(user_id),))
     DB.commit()
     return True
