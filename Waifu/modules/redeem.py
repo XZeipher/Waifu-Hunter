85d1ceeaf3844c1d8feaa38a5a3c5cfab691626d
XZeipher <144370232+XZeipher@users.noreply.github.com>
2024-03-27 12:25:57 +0530

    Update redeem.py

--- Waifu/modules/redeem.py
+++ Waifu/modules/redeem.py
@@ -2,6 +2,7 @@ from pyrogram import Client,filters
 from Waifu import *
 from Waifu.functions.watch_db import redeem_code
 
+alpha = []
 
 @Client.on_message(filters.command("redeem") & filters.private)
 async def redeemer(client,message):
@@ -17,13 +18,12 @@ async def redeemer(client,message):
         cusr.execute("SELECT * FROM codes WHERE code = %s",(code,))
         esep = cusr.fetchone()
         name = esep[2]
-        cusr.execute("SELECT * FROM user_data WHERE name = %s",(name,))
-        result = cusr.fetchone()
+        if user_id in alpha:
+            return await temp.edit_text("Already redeemed.")
     except:
         return await temp.edit_text("Invalid Code")
-    if result:
-        return await temp.edit_text("Already redeemed.")
     request = await redeem_code(code,user_id)
     if not request:
         return await temp.edit_text("Invalid Code")
+    alpha.append(user_id)
     return await temp.edit_text(f"Successfully Redeemed {name}")
