c41ecf5132785f2e45cd752db1dcbca193e9299e
XZeipher <144370232+XZeipher@users.noreply.github.com>
2024-03-27 11:34:15 +0530

    Update redeem.py

--- Waifu/modules/redeem.py
+++ Waifu/modules/redeem.py
@@ -10,10 +10,13 @@ async def redeemer(client,message):
     if not message.text.split(maxsplit=1)[1]:
         return await temp.edit_text("Enter the redeem code.")
     code = message.text.split(maxsplit=1)[1].strip()
-    cusr.execute("SELECT * FROM codes WHERE code = %s",(code,))
-    esep = cusr.fetchone()
-    cusr.execute("SELECT * FROM user_data WHERE pic = %s",(esep[5],))
-    result = cusr.fetchone()
+    try:
+        cusr.execute("SELECT * FROM codes WHERE code = %s",(code,))
+        esep = cusr.fetchone()
+        cusr.execute("SELECT * FROM user_data WHERE pic = %s",(esep[5],))
+        result = cusr.fetchone()
+    except:
+        return await temp.edit_text("Invalid Code")
     if result:
         return await temp.edit_text("Already redeemed.")
     request = await redeem_code(code,user_id)
