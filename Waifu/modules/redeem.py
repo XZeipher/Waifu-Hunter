30929867a7d02177ec4313f9578e54a24b4a403c
XZeipher <144370232+XZeipher@users.noreply.github.com>
2024-03-27 11:31:34 +0530

    Create redeem.py

---
+++ Waifu/modules/redeem.py
@@ -0,0 +1,22 @@
+from pyrogram import Client,filters
+from Waifu import *
+from Waifu.functions.watch_db import redeem_code
+
+
+@Client.on_message(filters.command("redeem") & filters.private)
+async def redeemer(client,message):
+    user_id = message.from_user.id
+    temp = await message.reply_text("processing....")
+    if not message.text.split(maxsplit=1)[1]:
+        return await temp.edit_text("Enter the redeem code.")
+    code = message.text.split(maxsplit=1)[1].strip()
+    cusr.execute("SELECT * FROM codes WHERE code = %s",(code,))
+    esep = cusr.fetchone()
+    cusr.execute("SELECT * FROM user_data WHERE pic = %s",(esep[5],))
+    result = cusr.fetchone()
+    if result:
+        return await temp.edit_text("Already redeemed.")
+    request = await redeem_code(code,user_id)
+    if not request:
+        return await temp.edit_text("Invalid Code")
+    return await temp.edit_text(f"Successfully Redeemed {esep[2]}")
