baf534a53b9e9ff9615996294fe1ba06618d8382
XZeipher <144370232+XZeipher@users.noreply.github.com>
2024-03-27 10:56:17 +0530

    Update adder.py

--- Waifu/modules/adder.py
+++ Waifu/modules/adder.py
@@ -118,7 +118,7 @@ async def adder(client , message:Message):
 
 @Client.on_message(filters.command("create") & filters.private & filters.user(6761575762))
 async def adder_code(client , message:Message):
-    user_id = msg.from_user.id
+    user_id = message.chat.id
     bot = message.chat
     rarity = None
     try:
@@ -130,7 +130,7 @@ async def adder_code(client , message:Message):
         link = f"https://graph.org{pic[0]}"
         count = await bot.ask('**Send The Number of waifus 💮**',filters=filters.text)
         key = ''.join(random.choices(string.ascii_uppercase + string.digits, k=10))
-        await new_code(key,user_id,name,anime,link,count)
+        await new_code(key,str(user_id),name,anime,link,count)
         await client.send_photo(chat_id=message.chat.id,photo=link,caption=create.format(message.from_user.mention,name.text.title(),anime.text.title(),key),reply_markup=cli_keyboard) 
         return
     except Exception as e:
